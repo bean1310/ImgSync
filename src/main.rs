361a90b594d10f23cd06e45b2fbad0c88c2fd215
Isato Matsushita <22887835+bean1310@users.noreply.github.com>
2021-05-31 04:43:08 +0900

    [feat] add option "--help" & "--daemon"

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate notify;
 
+use std::env;
+
 use notify::{RecommendedWatcher, Watcher, RecursiveMode};
 use std::sync::mpsc::channel;
 use std::time::Duration;
@@ -56,14 +58,34 @@ fn main()
     
     let observe_dir = config["basic"]["dir"].clone().unwrap();
     let token       = config["slack"]["token"].clone().unwrap();
-    // This program run as daemon process
-    let daemon = init_daemon();
 
-    if let Err(e) = daemon {
-        eprintln!("Error happned, {}", e);
-        panic!();
+    // Option handling
+    let _arg_len = env::args().len();
+    if _arg_len > 0 {
+        let _args: Vec<String> = env::args().collect();
+
+        let _help_flag = _args.iter().any(|options| options == "--help");
+        let _daemon_flag = _args.iter().any(|options| options == "--daemon");
+
+        if _help_flag {
+            println!("Image Syncer written in Rust\n");
+            println!("USAGE:\n\timg_sync [OPTIONS]\n");
+            println!("OPTIONS:\n\t--help\t\tPrint help information\n\t--daemon\tRun as daemon process");
+            std::process::exit(0);
+        }
+
+        if _daemon_flag {
+            // If given "--daemon" flag, this program run as daemon process
+            let daemon = init_daemon();
+
+            if let Err(e) = daemon {
+                eprintln!("Error happned, {}", e);
+                panic!();
+            }
+        }
     }
 
+    // Start main process
     if let Err(e) = watch(&observe_dir) {
         println!("error: {:?}", e);
         panic!()
@@ -88,3 +110,5 @@ fn watch(observe_dir: &str) -> notify::Result<()>
     }
 
 }
+
+// fn upload
